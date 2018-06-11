3ae4be1dd211e17a584e5e28a7ad0c4ff8e7f547
helen <helencho@ac.c4q.nyc>
2018-06-11 11:49:54 -0400

    prevent player from playing game after game is over

--- src/App.js
+++ src/App.js
@@ -48,11 +48,9 @@ class App extends Component {
     for (let x = 0; x < 6; x++) {
       if (x === 0 && this.state.board[x][col]) {
         // If user chooses 1st row and it's filled, return first row 
-        // console.log(`Column is full!`)
         return null
       } else if (this.state.board[x][col]) {
         // If we find a row that's been filled, return the previous row 
-        // console.log(`Dropping at ${x - 1}`)
         return x - 1
       } else if (x === 5) {
         // If we're at the last row, return last row 
@@ -99,38 +97,41 @@ class App extends Component {
 
   // When user clicks on column, drop piece on the next available row 
   handleClick = (row, col) => {
-    let newBoard = [...this.state.board]
-    let dropRow = this.findRow(col)
-    let turn = this.state.turn
+    const { gameOver } = this.state
+    if (!gameOver) {
+      let newBoard = [...this.state.board]
+      let dropRow = this.findRow(col)
+      let turn = this.state.turn
 
-    if (dropRow !== null) {
-      if (turn === 'red') {
-        newBoard[dropRow][col] = redPiece
-        turn = 'white'
-      } else {
-        newBoard[dropRow][col] = whitePiece
-        turn = 'red'
+      if (dropRow !== null) {
+        if (turn === 'red') {
+          newBoard[dropRow][col] = redPiece
+          turn = 'white'
+        } else {
+          newBoard[dropRow][col] = whitePiece
+          turn = 'red'
+        }
+        this.setState({
+          board: newBoard
+        })
       }
-      this.setState({
-        board: newBoard
-      })
-    }
 
-    let victory = this.checkVictory(dropRow, col)
+      let victory = this.checkVictory(dropRow, col)
 
-    if (victory) {
-      this.setState({
-        gameOver: true
-      })
-    } else {
-      this.setState({
-        turn
-      })
+      if (victory) {
+        this.setState({
+          gameOver: true
+        })
+      } else {
+        this.setState({
+          turn
+        })
+      }
     }
   }
 
   render() {
-    const { gameOver, turn } = this.state 
+    const { gameOver, turn } = this.state
 
     return (
       <div>
